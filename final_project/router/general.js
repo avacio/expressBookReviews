e37ba584ebd9beaf58b83c461467725fa37ed0bc
avacio <lexigerv@gmail.com>
2025-07-21 16:26:40 -0700

    Some HTTP tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const jsonSpaces = 4;
 
 
 public_users.post("/register", (req,res) => {
@@ -13,25 +14,55 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  return res.status(200).json(JSON.stringify(books, null, jsonSpaces));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  // lowercase comparison
+  const author = req.params.author.toLowerCase();
+  let result = [];
+
+  console.log('Querying Author: ' + author);
+  for (const book in books) {
+    // Lowercase comparison
+    if (books[book].author.toLowerCase() == author) {
+      result.push(JSON.stringify(books[book]));
+    }
+  }
+
+  return res.send(JSON.stringify(result, null, jsonSpaces));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  // lowercase comparison
+  const title = req.params.title.toLowerCase();
+  let result = [];
+
+  console.log('Querying Title: ' + title);
+  for (const book in books) {
+    // Lowercase comparison
+    if (books[book].title.toLowerCase() == title) {
+      result.push(JSON.stringify(books[book]));
+    }
+  }
+
+  return res.send(JSON.stringify(result, null, jsonSpaces));
 });
 
 //  Get book review

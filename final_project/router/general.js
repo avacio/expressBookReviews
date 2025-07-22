8e24aff380dba8a5282e70020e1a6396660e7422
avacio <lexigerv@gmail.com>
2025-07-22 02:40:16 -0700

    Task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,11 +4,11 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 const jsonSpaces = 4;
+const promiseTimeoutDuration = 6000;
 
 const stripSpaces = (string) => {
   return string.replaceAll(" ", "").replaceAll("+", "");
 }
-
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -31,19 +31,47 @@ public_users.post("/register", (req,res) => {
   return res.status(404).json({ message: "Unable to register user." });
 });
 
+function stringifyAndResolve(data) {
+  return new Promise((resolve, reject) => {
+    try {
+      const jsonString = JSON.stringify(data, null, jsonSpaces);
+      resolve(jsonString);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-
-  return res.status(200).json(JSON.stringify(books, null, jsonSpaces));
+  stringifyAndResolve(books)
+    .then(jsonResult => {
+      return res.status(200).json(jsonResult);
+    })
+    .catch(error => {
+      console.error("Error stringifying data:", error);
+      return res.status(404).json("Error stringifying books");
+    });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
 
-  const isbn = req.params.isbn;
-  res.send(books[isbn]);
+  const isbnPromise = new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+
+    if (books[isbn]) {
+      resolve(books[isbn]);
+    } else {
+      reject({ status: 404, message: `ISBN ${isbn} not found` });
+    }
+  });
+
+  isbnPromise.then(result => res.send(result),
+    error => res.status(error.status).json({ message: error.message })
+  );
  });
   
 // Get book details based on author
